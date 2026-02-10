bb8d816b009cc279a9e92f1aa9680d9492386c24
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-02-10 15:33:38 +0200

    add code to class Main1

--- TestJava/src/Main1.java
+++ TestJava/src/Main1.java
@@ -1,2 +1,26 @@
-public class Main1 {
+public class Main {
+    public static void main(String[] args) {
+        Director director = new Director();
+
+        // 1. Створюємо білдер для звичайного студента
+        StudBuilder normalBuilder = new NormalStudBuilder();
+        director.setBuilder(normalBuilder);
+
+        // 2. Створюємо двох студентів в ОДНОМУ університеті (перевірка Flyweight)
+        System.out.println("--- Створення студентів ---");
+        Student st1 = director.constructStudent("Іван", "Петренко", "КПІ");
+        Student st2 = director.constructStudent("Олена", "Коваль", "КПІ"); // "КПІ" не створиться вдруге
+        Student st3 = director.constructStudent("Макс", "Сидорчук", "НАУ"); // Створиться новий об'єкт університету
+
+        System.out.println("\n--- Перевірка роботи ---");
+        st1.doActivity();
+        st2.doActivity();
+        st3.doActivity();
+
+        // 3. Перевірка State (змінюємо стан першого студента)
+        System.out.println("\n--- Зміна стану (Івана відраховують) ---");
+        st1.changeState(new ExpelledState());
+        st1.doActivity(); // Поведінка зміниться
+        st1.payTuition(); // Поведінка зміниться
+    }
 }
