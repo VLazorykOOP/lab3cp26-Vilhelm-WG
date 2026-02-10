583a92004c073ecce4e09ce2bdb7f4124e47ef72
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-02-10 15:30:53 +0200

    add code to class StudentState

--- TestJava/src/StudentState.java
+++ TestJava/src/StudentState.java
@@ -1,2 +1,33 @@
-public class StudentState {
+// State Interface
+interface StudentState {
+    void study();
+    void payTuition();
+}
+
+// Concrete State 1: На навчанні
+class ActiveState implements StudentState {
+    @Override
+    public void study() {
+        System.out.println("Студент відвідує пари.");
+    }
+    @Override
+    public void payTuition() {
+        System.out.println("Оплата за семестр прийнята.");
+    }
+    @Override
+    public String toString() { return "Активний"; }
+}
+
+// Concrete State 2: Відрахований
+class ExpelledState implements StudentState {
+    @Override
+    public void study() {
+        System.out.println("Студент не може вчитися, його відраховано.");
+    }
+    @Override
+    public void payTuition() {
+        System.out.println("Немає за що платити.");
+    }
+    @Override
+    public String toString() { return "Відрахований"; }
 }
