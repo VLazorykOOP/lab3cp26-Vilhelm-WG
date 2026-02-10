d63d04d80313c8a43002c592d59d9468503b55b7
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-02-10 15:30:04 +0200

    add code to class University

--- TestJava/src/University.java
+++ TestJava/src/University.java
@@ -1,2 +1,30 @@
-public class University {
+import java.util.HashMap;
+import java.util.Map;
+
+// Flyweight: Спільний об'єкт (наприклад, Університет)
+class University {
+    private String name;
+
+    public University(String name) {
+        this.name = name;
+        System.out.println("Створено новий університет: " + name);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+// Flyweight Factory: Керує пулом університетів
+class UniversityFactory {
+    private static final Map<String, University> universities = new HashMap<>();
+
+    public static University getUniversity(String name) {
+        University uni = universities.get(name);
+        if (uni == null) {
+            uni = new University(name);
+            universities.put(name, uni);
+        }
+        return uni;
+    }
 }
